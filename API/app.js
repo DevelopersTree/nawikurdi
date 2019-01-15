39be91395bde0521be214762825478e65941213e
aram rafeq <aramrafeq2@gmail.com>
2019-01-15 21:29:31 +0300

    api link and error displaying fixed

--- API/app.js
+++ API/app.js
@@ -18,16 +18,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- API/routes/index.js
+++ API/routes/index.js
@@ -4,14 +4,13 @@ var db=require('../config');
 
 router.get('/:limit/:offset', function(req, res, next) {
   db('names').select().where({Deleted:0,Activated:1}).limit(parseInt(req.params.limit)).offset(parseInt(req.params.offset)).then(function(names){
-    res.json(names);
+    res.status(200).json(names);
   });
 });
 
 router.get('/numberOffRecord', function(req, res, next) {
   db('names').count({ numberOffRecord: 'nameId' }).where({Deleted:0,Activated:1}).then(function(numberOffRecord){
-    console.log(numberOffRecord);
-    res.json(numberOffRecord[0]);
+    res.status(200).json(numberOffRecord[0]);
   });
 });
 
@@ -23,7 +22,7 @@ router.get('/:limit/:offset/:searchValue/:dropdwon', function(req, res, next) {
   if(req.params.dropdwon=='F' || req.params.dropdwon=='M' || req.params.dropdwon=='O'){
     db('names').select().where({Deleted:0,Activated:1,gender:req.params.dropdwon}).whereRaw("name like ?", "%"+searchValueEdit+"%").limit(parseInt(req.params.limit)).offset(parseInt(req.params.offset)).then(function(names){
       db('names').count({ numberOffRecord: 'nameId' }).where({Deleted:0,Activated:1,gender:req.params.dropdwon}).whereRaw("name like ?", "%"+searchValueEdit+"%").then(function(numberOffRecord){
-        res.json({
+        res.status(200).json({
         names:names,
         numberOffRecord:numberOffRecord[0]});
       });
@@ -31,7 +30,7 @@ router.get('/:limit/:offset/:searchValue/:dropdwon', function(req, res, next) {
   }else{
     db('names').select().where({Deleted:0,Activated:1}).whereRaw("name like ?", "%"+searchValueEdit+"%").limit(parseInt(req.params.limit)).offset(parseInt(req.params.offset)).then(function(names){
     db('names').count({ numberOffRecord: 'nameId' }).where({Deleted:0,Activated:1}).whereRaw("name like ?", "%"+searchValueEdit+"%").limit(parseInt(req.params.limit)).offset(parseInt(req.params.offset)).then(function(numberOffRecord){
-        res.json({
+        res.status(200).json({
         names:names,
         numberOffRecord:numberOffRecord[0]});
       });
@@ -45,7 +44,7 @@ router.post('/addNewName', function(req, res, next) {
       desc:req.body.meaningSendName,
       gender:req.body.gender
     }).then(function(){
-      res.json({status:1})
+      res.status(200).json({status:1})
     }).catch(function(err){
       res.json(err)
     });
